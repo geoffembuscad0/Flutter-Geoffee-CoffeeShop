851f6fb0d8ac713f4c27ea485ca87c3698fe5708
geoffembuscad0 <geoff.embuscado@yahoo.com.ph>
2023-03-27 12:13:50 +0800

    Added model for Coffee item.

--- lib/model/coffee_model.dart
+++ lib/model/coffee_model.dart
@@ -1,1 +1,33 @@
+final String tableCoffee = 'coffee';
 
+class CoffeeFields {
+  static final List<String> values = [
+    id, name, baseSrp
+  ];
+
+  static final String id = 'id';
+  static final String name = 'name';
+  static final String baseSrp = 'baseSrp';
+}
+
+class Coffee {
+  final int? id;
+  final String? name;
+  final double? baseSrp;
+
+  const Coffee({
+    this.id,
+    required this.name,
+    required this.baseSrp,    
+  });
+
+  Coffee copy({
+    int? id,
+    String? name,
+    double? baseSrp
+  }) => Coffee(
+    id: id ?? this.id,
+    name: name ?? this.name,
+    baseSrp: baseSrp ?? this.baseSrp,
+  );
+}
